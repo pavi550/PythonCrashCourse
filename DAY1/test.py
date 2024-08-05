93efd38d5f9c95a2466a074fabf5bccad213c50b
pavi550 <pavi550@gmail.com>
2024-08-05 12:41:35 +0530

    tt

--- DAY1/test.py
+++ DAY1/test.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #Assigning multiple values to multiple variables
 
 a = 50
@@ -9,3 +10,13 @@ print("The type of b", type(b))
 c = 1 + 3
 i
 print("The type of c", type(c))
+=======
+a = 50  
+print("The type of a", type(a))  
+  
+b = 40.5  
+print("The type of b", type(b))  
+  
+c = 1+3i  
+print("The type of c", type(c))  
+>>>>>>> 4da0219 (12)
