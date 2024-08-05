cf498011ed4b40467c49654b1c430f106b73d2e2
pavi550 <pavi550@gmail.com>
2024-08-05 22:55:54 +0530

    3

--- DAY1/assign3.py
+++ DAY1/assign3.py
@@ -1,7 +1,35 @@
+import sys
 
-from ArithmeticOperators import add
+def add (num1,num2):
+    sum=num1+num2
+    return sum
+
+def sub(num1,num2) :
+    sub=num1-num2
+    return sub
+
+def mul(num1,num2):
+    mul=num1*num2
+    return mul
+
+def div(num1,num2) :
+    div=num1/num2
+    return div
 
 
-def add (argv[1],operation,argv[2]):
-    sum=int(a+b)
-print("Sum of two no=",sum)
+num1=float (sys.argv[1])
+operation=sys.argv[2]
+num2=float(sys.argv[3])
+
+if operation=="add" :
+    output=add(num1,num2)
+    print(output)
+elif operation=="sub" :
+        result=sub(num1,num2)
+        print(result)
+elif operation =="mul" :
+    result=mul(num1,num2)
+    print(result)
+else :
+    result=div(num1,num2)
+    print(result)
